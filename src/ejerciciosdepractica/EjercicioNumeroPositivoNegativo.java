f3824499a94d07b1dffe4c60937d50611af07e13
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-06-05 15:04:36 -0600

    Modificaciones en el segundo ejercicio

--- src/ejerciciosdepractica/EjercicioNumeroPositivoNegativo.java
+++ src/ejerciciosdepractica/EjercicioNumeroPositivoNegativo.java
@@ -1,7 +1,6 @@
 /*
 INSTRUCCIONES: Leer un número e indicar si es positivo o negativo. El proceso se repetirá hasta que se introduzca
 un 0.
-
 */
 
 package ejerciciosdepractica;
@@ -14,11 +13,14 @@ public class EjercicioNumeroPositivoNegativo {
         
         Scanner teclado = new Scanner(System.in);
         
+        // Le damos la bienvenida al usuario
         System.out.println("PROGRAMA PARA SABER SI UN NUMERO ES POSITIVO O NEGATIVO");
 
-        
+        // Declaramos la variable a comparar para saber si es positiva o negativa
         int numero = 1;
         
+        
+        // Usamos un ciclo while que compruebe el valor y se repita hasta que el usuario ingrese 0
         while(numero != 0){
             System.out.println("Ingresa un numero: ");
             numero = teclado.nextInt();
